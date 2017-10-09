4bf2949b1a68e60f1f7b52109ac304efbc423507
ggrijalva <ggrijalva@users.noreply.github.com>
2017-10-09 16:35:14 -0500

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -5,7 +5,6 @@ function leeCatalogoCsv(File, callback) {
  var request = window.ActiveXObject ?
      new ActiveXObject('Microsoft.XMLHTTP') :
      new XMLHttpRequest;
-
  request.onreadystatechange = function() {
    if (request.readyState == 4 && request.status==200) {
      //request.onreadystatechange = doNothing;
@@ -37,7 +36,7 @@ var mapOptions = {
 	// Se define el mapa					
 	map = new google.maps.Map(document.getElementById("map3"),mapOptions); // Para definir como variable global
 	// Cargar las estaciones siguientes
-	leeCatalogoCsv("CatalogoConchos.csv", function(data) {
+	leeCatalogoCsv("CatalogoConchos.csv", function(data){
 		var allRows = data.split(/\r\n|\r/);
 		var Marcadores = Array();
 		var infowindowActivo = false;
@@ -71,13 +70,14 @@ var mapOptions = {
 						infowindowActivo.close();
 					infowindowActivo = this.infoWindow;
 					infowindowActivo.open(map, this);
-
 				});
 				// Aquí se generan los puntos a cargar  
 				Marcadores.push(mark);
 			} // Cierre del if para que solo cargue aquellos con coordenadas 
 		  } // Cierre del for
-	});
+	// Cargar la tabla con los datos tambien
+	successFunction(data);
+	}); // Cierre de función que usa ajax
 } 
 //////////////////////////////////////////
 // Tabla para el archivo CSV
@@ -88,20 +88,30 @@ function successFunction(data) {
   for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
     if (singleRow === 0) {
       table += '<thead>';
-      table += '<tr>';
-    } else {
+      table += '<tr>';   }
+    else {
       table += '<tr>';
     }
     var rowCells = allRows[singleRow].split(',');
     for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
       if (singleRow === 0) {
-        table += '<th>';
-        table += rowCells[rowCell];
-        table += '</th>';
-      } else {
-        table += '<td>';
-        table += rowCells[rowCell];
-        table += '</td>';
+        if (rowCell > 1 | rowCell == 0){
+	        table += '<th>';
+   	     table += rowCells[rowCell];
+      	  table += '</th>';
+      	}
+      } 
+      if (singleRow != 0) {
+      	if (rowCell == 0){
+	        table += '<td>';
+   	     table += '<a href="ConsultaEst.html?Est='+ rowCells[rowCell] +'">' + rowCells[rowCell] + '</a>';
+      	  table += '</td>';
+      	}
+			if (rowCell > 1){
+	        table += '<td>';
+   	     table += rowCells[rowCell];
+      	  table += '</td>';
+      	}
       }
     }
     if (singleRow === 0) {
@@ -122,3 +132,161 @@ function loadCsv() {
 		successFunction(data);}
 	);
 }
+///////////////////////////////////////
+// Darken a color
+function darkenColor(colorStr) {
+  // Defined in dygraph-utils.js
+  var color = Dygraph.toRGB_(colorStr);
+  color.r = Math.floor((255 + color.r) / 2);
+  color.g = Math.floor((255 + color.g) / 2);
+  color.b = Math.floor((255 + color.b) / 2);
+  return 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')';
+}
+
+// This function draws bars for a single series. See
+// multiColumnBarPlotter below for a plotter which can draw multi-series
+// bar charts.
+function barChartPlotter(e) {
+  var ctx = e.drawingContext;
+  var points = e.points;
+  var y_bottom = e.dygraph.toDomYCoord(0);
+
+  ctx.fillStyle = darkenColor(e.color);
+  // Find the minimum separation between x-values.
+  // This determines the bar width.
+  var min_sep = Infinity;
+  for (var i = 1; i < points.length; i++) {
+    var sep = points[i].canvasx - points[i - 1].canvasx;
+    if (sep < min_sep) min_sep = sep;
+  }
+  var bar_width = Math.floor(3.0 / 4 * min_sep);
+  // Do the actual plotting.
+  for (var i = 0; i < points.length; i++) {
+    var p = points[i];
+    var center_x = p.canvasx;
+    ctx.fillRect(center_x - bar_width / 2, p.canvasy,
+        bar_width, y_bottom - p.canvasy);
+    ctx.strokeRect(center_x - bar_width / 2, p.canvasy,
+        bar_width, y_bottom - p.canvasy);
+  }
+}
+/////////////////////////////////////////////////
+// Funcion de convertir Csv a arreglo javascript para Dygraphs
+/*var toArray = function(data) {
+  var lines = data.split("\n");
+  var arry = [];
+  for (var idx = 0; idx < lines.length; idx++) {
+    var line = lines[idx];
+    // Oftentimes there's a blank line at the end. Ignore it.
+    if (line.length == 0) {
+      continue;
+    }
+    var row = line.split(",");
+    // Special processing for every row except the header.
+    if (idx >= 0) {
+      row[0] = Date.parse(row[0] + " 00:00"); // Turn the string date into a Date.
+      for (var rowIdx = 1; rowIdx < row.length; rowIdx++) {
+        // Turn "123" into 123.
+       // if (row[rowIdx] == ""){row[rowIdx] = "";}
+       // if (row[rowIdx] != ""){row[rowIdx] = parseFloat(row[rowIdx]);} 
+        row[rowIdx] = parseFloat(row[rowIdx])
+      }     
+    }     
+    arry.push(row);
+  }     
+  return arry;
+}
+/////////////////////////////////////////////////
+// Funcion de convertir Csv a arreglo sin formato de fecha o NaN para 
+var toArraysf = function(data) {
+  var lines = data.split("\n");
+  var arry = [];
+  for (var idx = 0; idx < lines.length; idx++) {
+    var line = lines[idx];
+    // Oftentimes there's a blank line at the end. Ignore it.
+    if (line.length == 0) {
+      continue;
+    }
+    var row = line.split(",");
+    // Special processing for every row except the header.
+    if (idx >= 0) {
+      //row[0] = Date.parse(row[0] + " 00:00"); // Turn the string date into a Date.
+      for (var rowIdx = 1; rowIdx < row.length; rowIdx++) {
+        // Turn "123" into 123.
+        if (row[rowIdx] == ""){row[rowIdx] = "";}
+        if (row[rowIdx] != ""){row[rowIdx] = parseFloat(row[rowIdx]);} 
+      }     
+    }     
+    arry.push(row);
+  }     
+  return arry;
+}
+///////////////////////////////////////////////////////
+// Extrae columna de un arreglo
+ function getColDate(matrix, col){
+    var column = [];
+    for(var i=0; i<matrix.length; i++){
+       column.push(Date.parse(matrix[i][col] + " 00:00"));
+    }
+    return column;
+ }
+ ////////////////////////////////////////
+ // Funciones para extraer indice del primer valor encontrado sobre las fechas graficadas
+ function getIndxIni(matrix, valor){
+  	valor = parseFloat(valor);  
+    for(var i = 0; i < matrix.length; i++){
+       if (valor <= matrix[i]){return i;}
+    }
+ }
+ 
+ function getIndxFin(matrix, valor){
+  	valor = parseFloat(valor);  
+    for(var i = matrix.length - 1; i > 0; i--){
+       if (valor >= matrix[i]){return i;}
+    }
+ }
+ ////////////////////////////////////////
+ // Extrae columna de un arreglo
+ function getCol(matrix, col){
+    var column = [];
+    for(var i=0; i < matrix.length; i++){
+       column.push(matrix[i][col]);
+    }
+    return column;
+ }
+ //////////////////////////////////////////
+ // Tabla html desde array
+ function tableHtml(matrix){
+ 	//var table = "<table id = 'tableTest' class = 'table-bordered table-hover center-block'>\n";
+ 	var	 table = "\n<thead>\n<tr><th>Fecha</th>\n<th>Precipitación (mm)</th>\n<th>Temp.Máxima (°C)</th>\n<th>Temp.Mínima (°C)</th>\n</tr></thead>\n<tbody>";
+    for(var i = 0; i < matrix.length; i++){
+    	var row = matrix[i];
+    	table += "<tr>\n"
+    	for(var j = 0; j < row.length; j++){
+       table += "<td>"+ row[j] +"</td>\n";
+    	}
+    	table += "</tr>\n"
+ 	 }
+ 	 //table += "</tbody>\n</table>";
+ 	 table += "</tbody>\n";
+    return table;
+ }
+//////////////////////////////////////////////////
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    "non-empty-string-asc": function (str1, str2) {
+        if(str1 == "")
+            return 1;
+        if(str2 == "")
+            return -1;
+        return ((parseFloat(str1) < parseFloat(str2)) ? -1 : ((parseFloat(str1) > parseFloat(str2)) ? 1 : 0));
+    },
+ 
+    "non-empty-string-desc": function (str1, str2) {
+        if(str1 == "")
+            return 1;
+        if(str2 == "")
+            return -1;
+        return ((parseFloat(str1) < parseFloat(str2)) ? 1 : ((parseFloat(str1) > parseFloat(str2)) ? -1 : 0));
+    }
+} );
+*/
